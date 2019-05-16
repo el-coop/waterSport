bf168d4aa16a5e8014d87f78e1b8cf27525ff6e4
Cucurrucho <adam.bar91@gmail.com>
2019-05-16 13:13:52 -0700

    hasFields package now online not local

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,11 +2,9 @@
 
 namespace App\Models;
 
-use ElCoop\HasFields\HasFields;
 use Illuminate\Database\Eloquent\Model;
 
 class Admin extends Model {
-	use HasFields;
 	public function user() {
 		return $this->morphOne(User::class, 'user');
 	}
