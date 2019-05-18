1b2525a648561d56a5d8935e87775bfeffd9d64e
Nur Bar <LCD344@yahoo.com>
2019-05-17 17:01:05 -0700

    Added admin user, autoloaded routing, sports model, datatable, usertype middleware

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
 	return view('welcome');
 });
 
+Route::get('datatable/list','\ElCoop\Datatable\Controllers\DatatableController@list');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
