ce2673e91c0ef5b3b812effbacf2eb514e310a63
Cucurrucho <adam.bar91@gmail.com>
2019-06-10 15:37:23 +0300

    added file management

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,12 +4,14 @@ namespace App\Providers;
 
 use App\Models\Admin;
 use App\Models\Competitor;
+use App\Models\Pdf;
 use App\Models\PracticeDay;
 use App\Models\Sport;
 use App\Models\SportField;
 use App\Models\SportManager;
 use App\Policies\CompetitorPolicy;
 use App\Policies\FieldPolicy;
+use App\Policies\PdfPolicy;
 use App\Policies\PracticeDayPolicy;
 use App\Policies\SportFieldPolicy;
 use App\Policies\SportManagerPolicy;
@@ -30,7 +32,8 @@ class AuthServiceProvider extends ServiceProvider {
 		SportField::class => SportFieldPolicy::class,
 		Competitor::class => CompetitorPolicy::class,
 		Field::class => FieldPolicy::class,
-		SportManager::class => SportManagerPolicy::class
+		SportManager::class => SportManagerPolicy::class,
+		Pdf::class => PdfPolicy::class
 	];
 	
 	/**
