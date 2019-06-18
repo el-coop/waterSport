6ad31daa0be3a6dfd464281c9c75cbc26ac8aa2c
aliciaderuijter <aliciaderuijter@gmail.com>
2019-06-18 16:37:27 +0200

    Added translations

--- resources/lang/nl/admin/settings.php
+++ resources/lang/nl/admin/settings.php
@@ -3,19 +3,19 @@ return [
 	'settings' => 'Instellingen',
 	'use' => 'Gebruik',
 	
-	'login_text_en' => 'Login tekst EN',
-	'login_text_nl' => 'Login tekst NL',
+	'login_text_en' => 'Tekst rechterblok op inlogpagina EN',
+	'login_text_nl' => 'Tekst rechterblok op inlogpagina NL',
 	'files' => 'Documenten',
 	'chooseFile' => 'Kies een bestand',
 	
-	'registration_success_en' => 'Registratie succesvol EN',
-	'registration_success_nl' => 'Registratie succesvol NL',
+	'registration_success_en' => 'Tekst banner inlogpagina na succesvolle registratie EN',
+	'registration_success_nl' => 'Tekst banner inlogpagina na succesvolle registratie NL',
 	
-	'registration_email_subject_en' => 'Registratie email onderwerp EN',
-	'registration_email_subject_nl' => 'Registratie email onderwerp NL',
+	'registration_email_subject_en' => 'Email na succesvolle registratie (deelnemer) onderwerp EN',
+	'registration_email_subject_nl' => 'Email na succesvolle registratie (deelnemer) onderwerp NL',
 	
-	'registration_email_body_en' => 'Registratie email tekst EN',
-	'registration_email_body_nl' => 'Registratie email tekst NL',
+	'registration_email_body_en' => 'Email na succesvolle registratie tekst EN',
+	'registration_email_body_nl' => 'Email na succesvolle registratie tekst NL',
 	
 	'confirmation_submitted_email_subject_en' => 'Bevestiging email onderwerp EN',
 	'confirmation_submitted_email_subject_nl' => 'Bevestiging email onderwerp NL',
@@ -23,16 +23,16 @@ return [
 	'confirmation_submitted_email_body_en' => 'Bevestiging email tekst EN',
 	'confirmation_submitted_email_body_nl' => 'Bevestiging email onderwerp NL',
 
-	'sport_manager_registration_email_subject_en' => 'Sport manager registratie email onderwerp EN',
-	'sport_manager_registration_email_subject_nl' => 'Sport manager registratioe email onderwerp NL',
+	'sport_manager_registration_email_subject_en' => 'Email na succesvolle registratie (sportmanager) onderwerp EN',
+	'sport_manager_registration_email_subject_nl' => 'Email na succesvolle registratie (sportmanager) onderwerp NL',
 
-	'sport_manager_registration_email_body_en' => 'Sport manager registratie email tekst EN',
-	'sport_manager_registration_email_body_nl' => 'Sport manager registratie email tekst NL',
+	'sport_manager_registration_email_body_en' => 'Email na succesvolle registratie (sportmanager) tekst EN',
+	'sport_manager_registration_email_body_nl' => 'Email na succesvolle registratie (sportmanager) tekst NL',
 	
 	'confirmation_success_text_nl' => 'Deelnemer registratie succesvol tekst NL',
 	'confirmation_success_text_en' => 'Deelnemer registratie succesvol tekst EN',
 	
-	'application_success_modal_nl' => 'Tekst pop-up succesvol formulier ingediend  NL',
-	'application_success_modal_en' => 'Tekst pop-up succesvol  formulier ingediend EN',
+	'application_success_modal_nl' => 'Tekst rechterblok na succesvolle registratie NL',
+	'application_success_modal_en' => 'Tekst rechterblok na succesvolle registratie EN',
 
 ];
